d49b3847d9d1521a50e287a119e2c3ebb08dc6b6
Andrey <sekarew@gmail.com>
2023-08-04 23:51:01 +0300

    Start task2

---
+++ Task2/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите первое число: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
