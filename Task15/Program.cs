ea407816000d0847f87694550ab7d856df8a4061
Andrey <sekarew@gmail.com>
2023-08-05 17:22:27 +0300

    End DZ Seminar2

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// предположу что выходной день это суб и воскр. А неделя начинается с понедельника. 
+
+Console.Write("Введите цифру, обозначающую день недели (от 1 до7): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 5 && number <= 7 )
+{
+    Console.Write("Да");
+}
+else{
+    Console.Write("Нет");
+}
