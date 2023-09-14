f96389308080ad6b6918365681bfa4ad047381d9
Andrey <sekarew@gmail.com>
2023-09-14 21:35:43 +0300

    end_task59

--- Task59/Program.cs
+++ Task59/Program.cs
@@ -11,8 +11,8 @@
 // 2 6 7
 
 Console.Clear();
-Console.WriteLine($"Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.");
-Console.WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
+
+Console.WriteLine($"Введите размер массива m x n и диапазон случайных значений:");
 int m = InputNumbers("Введите m: ");
 int n = InputNumbers("Введите n: ");
 int range = InputNumbers("Введите диапазон: от 1 до ");
@@ -34,70 +34,70 @@ WriteArray(arrayWithoutLines);
 
 int InputNumbers(string input)
 {
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
 }
 
 void CreateArray(int[,] array)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      array[i, j] = new Random().Next(range);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
     }
-  }
 }
 
 void WriteArray(int[,] array)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      Console.Write(array[i, j] + " ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
-  }
 }
 
 int[,] FindPositionSmallElement(int[,] array, int[,] position)
 {
-  int temp = array[0, 0];
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    int temp = array[0, 0];
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      if (array[i, j] <= temp)
-      {
-        position[0, 0] = i;
-        position[0, 1] = j;
-        temp = array[i, j];
-      }
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] <= temp)
+            {
+                position[0, 0] = i;
+                position[0, 1] = j;
+                temp = array[i, j];
+            }
+        }
     }
-  }
-  Console.WriteLine($"\nMинимальный элемент: {temp}");
-  return position;
+    Console.WriteLine($"\nMинимальный элемент: {temp}");
+    return position;
 }
 
 void DeleteLines(int[,] array, int[,] positionSmallElement, int[,] arrayWithoutLines)
 {
-  int k = 0, l = 0;
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    int k = 0, l = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      if (positionSmallElement[0, 0] != i && positionSmallElement[0, 1] != j)
-      {
-        arrayWithoutLines[k, l] = array[i, j];
-        l++;
-      }
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (positionSmallElement[0, 0] != i && positionSmallElement[0, 1] != j)
+            {
+                arrayWithoutLines[k, l] = array[i, j];
+                l++;
+            }
+        }
+        l = 0;
+        if (positionSmallElement[0, 0] != i)
+        {
+            k++;
+        }
     }
-    l = 0;
-    if (positionSmallElement[0, 0] != i)
-    {
-      k++;
-    }
-  }
 }
